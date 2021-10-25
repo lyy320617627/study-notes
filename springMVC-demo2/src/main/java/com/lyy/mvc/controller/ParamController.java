e59f27fd757ab9f996172f66241637dbbdb729ba
lyy320617627 <ly320617627@163.com>
2021-10-25 19:55:00 +0800

    SpringMVC获取请求参数以及设置字符集乱码问题（在字符过滤器中设置）

--- springMVC-demo2/src/main/java/com/lyy/mvc/controller/ParamController.java
+++ springMVC-demo2/src/main/java/com/lyy/mvc/controller/ParamController.java
@@ -1,10 +1,14 @@
 package com.lyy.mvc.controller;
 
+import com.lyy.mvc.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 
 /**
@@ -18,6 +22,7 @@ public class ParamController {
     @RequestMapping("/testServletAPI")
     //形参位置的request表示当前请求
     public String testServletAPI(HttpServletRequest request){
+        HttpSession session = request.getSession();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         System.out.println("username:"+username+",password:"+password);
@@ -27,10 +32,20 @@ public class ParamController {
     public String testParam(
             @RequestParam(value = "user_name",required = false,defaultValue = "hehe")String username,
             String password,
-            String[] hobby){
+            String[] hobby,
+            @RequestHeader("Host") String host,
+            @CookieValue("JSESSIONID") String JSESSIONID
+            ){
         //若请求参数中出现多个请求参数，可以在控制器方法的形参位置设置字符串类型或字符串数组来接收此请求参数
         //若使用字符串类型的形参，最终结果为请求参数的每一个值之间使用逗号进行拼接
         System.out.println("username:"+username+",password:"+password+",hobby:"+ Arrays.toString(hobby));
+        System.out.println("host:"+host);
+        System.out.println("JSESSIONID:"+JSESSIONID);
+        return "success";
+    }
+    @RequestMapping("/testBean")
+    public String testBean(User user){
+        System.out.println(user);
         return "success";
     }
 }
