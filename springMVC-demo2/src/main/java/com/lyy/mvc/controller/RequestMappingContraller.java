a09f662631065ec6c275d30f268041f658bb623d
lyy320617627 <ly320617627@163.com>
2021-10-25 15:01:50 +0800

    RequestMapping请求的属性测试

--- springMVC-demo2/src/main/java/com/lyy/mvc/controller/RequestMappingContraller.java
+++ springMVC-demo2/src/main/java/com/lyy/mvc/controller/RequestMappingContraller.java
@@ -1,6 +1,8 @@
 package com.lyy.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,8 +18,37 @@ public class RequestMappingContraller {
   @RequestMapping(value = {
           "/testRequestMapping","/test"},
           method = {RequestMethod.POST}
+
   )
       public String success(){
         return "success";
       }
+      @GetMapping("/testGetMapping")
+      public String testGetMapping(){
+      return "success";
+      }
+      @RequestMapping(value = "/testPut",method =RequestMethod.PUT)
+    public String testPut(){
+      return "success";
+      }
+      @RequestMapping(
+              value="/testParamsAndHeaders",
+              params = {"username"},
+              headers = {"Host=localhost:8081"}
+      )
+    public String testParamsAndHeaders(){
+
+      return "success";
+      }
+//         @RequestMapping("/a*a/testAnt")
+         @RequestMapping("/**/testAnt")
+    public String testAnt(){
+      return "success";
+         }
+         @RequestMapping("/testPath/{id}/{username}")
+    public String testPath(@PathVariable("id")Integer id,@PathVariable("username") String username){
+             System.out.println("id:"+id);
+             System.out.println("username："+username);
+      return "success";
+         }
 }
