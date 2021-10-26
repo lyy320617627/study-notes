29a1e2c34335734d7da4fd71a9b76bf3ec30fb30
lyy320617627 <ly320617627@163.com>
2021-10-26 14:25:20 +0800

    SpringMVC域对象共享数据

--- springMVC-demo3/src/main/java/com/lyy/mvc/controller/ScopeController.java
+++ springMVC-demo3/src/main/java/com/lyy/mvc/controller/ScopeController.java
@@ -1,10 +1,14 @@
 package com.lyy.mvc.controller;
 
+import com.sun.javafx.collections.MappingChange;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * @program: springMVC
@@ -28,5 +32,20 @@ public class ScopeController {
         //设置视图名称
         mav.setViewName("success");
         return mav;
+    }
+    @RequestMapping("/testModel")
+    public String testModel(Model model){
+        model.addAttribute("testRequestScope","hello,model");
+        return "success";
+    }
+    @RequestMapping("/testMap")
+    public String testMap(Map<String,Object> map){
+        map.put("testRequestScope","hello,Map");
+        return "success";
+    }
+    @RequestMapping("/testModelMap")
+    public String testModelMap(ModelMap modelMap){
+        modelMap.addAttribute("testRequestScope","hello,ModelMap");
+        return  "success";
     }
 }
