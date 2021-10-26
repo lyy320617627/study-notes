9538a1476b592d9653c33b1bfef0e518b3f20968
lyy320617627 <ly320617627@163.com>
2021-10-26 15:56:46 +0800

    SpringMVC域对象共享数据

--- springMVC-demo3/src/main/java/com/lyy/mvc/controller/ScopeController.java
+++ springMVC-demo3/src/main/java/com/lyy/mvc/controller/ScopeController.java
@@ -7,7 +7,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 /**
@@ -48,4 +50,16 @@ public class ScopeController {
         modelMap.addAttribute("testRequestScope","hello,ModelMap");
         return  "success";
     }
+    @RequestMapping("/testSession")
+    public String testSession(HttpSession session){
+        session.setAttribute("testSessionScope","hello session");
+        return "success";
+    }
+    @RequestMapping("/testApplication")
+    public String testApplication(HttpSession session){
+        ServletContext application = session.getServletContext();
+        application.setAttribute("testApplicationScope","hello,application");
+        return "success";
+    }
+
 }
